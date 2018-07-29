b7ad48167f9a2358a48ca3bc98f4d039818381b3
tama1029 <testplayers@gmail.com>
2018-07-29 22:17:55 +0900

    why jsx

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to React Hoge</h1>
-      </div>
+      <h2>
+        Welcome to React Hoge
+      </h2>
     );
   }
 }
