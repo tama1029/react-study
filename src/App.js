07a07d247532f259e7fb6cddcd9a49a731b38459
tama1029 <testplayers@gmail.com>
2018-07-29 22:48:36 +0900

    learn react fragment

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <h2>
-        Welcome to React Hoge
-      </h2>
-    );
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" name="huga" onClick={() => {console.log("click")}}/>
+      </React.Fragment>
+    )
   }
 }
-
 export default App;
